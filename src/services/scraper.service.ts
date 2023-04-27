30fdc70958938c09d87176e3a421b0aa467ca874
ad <39013925+ad2017gd@users.noreply.github.com>
2023-04-27 14:42:05 +0300

    Fixed title scraper

--- src/services/scraper.service.ts
+++ src/services/scraper.service.ts
@@ -198,7 +198,7 @@ export default class ScraperService {
 
     // region - Show Info
     private scrapShowName($: cheerio.Root): string {
-        const value = $('h1[class^="TitleHeader"]').text();
+        const value = $('h1[data-testid="hero__pageTitle"]').text();
         return decode(value).trim();
     }
 
